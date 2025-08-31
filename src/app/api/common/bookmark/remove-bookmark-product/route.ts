8e4ae6ff6840aa78fd086486b35e4d275144939e
Kuna Jaganmohan Rao <145915079+22L31A0497@users.noreply.github.com>
2025-08-31 21:16:32 +0530

    Update route.ts

--- src/app/api/common/bookmark/remove-bookmark-product/route.ts
+++ src/app/api/common/bookmark/remove-bookmark-product/route.ts
@@ -1,4 +1,4 @@
-import connectDB from "@/app/components/DB/connectDB";
+import connectDB from "../../../../../lib/connectDB";
 import AuthCheck from "../../../../../middleware/AuthCheck";
 import { NextResponse } from "next/server";
 import Bookmark from "../../../../../model/Bookmark";
