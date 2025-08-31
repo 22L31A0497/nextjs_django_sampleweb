ae539e853d26d363aa8f9032a1be861edfe8f8fc
Kuna Jaganmohan Rao <145915079+22L31A0497@users.noreply.github.com>
2025-08-31 21:01:30 +0530

    Update route.ts

--- src/app/api/Admin/order/update-order-status/route.ts
+++ src/app/api/Admin/order/update-order-status/route.ts
@@ -1,4 +1,4 @@
-import connectDB from "@/app/components/DB/connectDB";
+import connectDB from "@/lib/connectDB";
 import AuthCheck from "../../../../../middleware/AuthCheck";
 import { NextResponse } from "next/server";
 import Order from "../../../../../model/Order";
@@ -10,32 +10,23 @@ export async function PUT(req: Request) {
 
     if (isAuthenticated === 'admin') {
       const data = await req.json();
-      const  id = data
-      console.log(id)
-      if(!id) return NextResponse.json({ success: false, message: "Please provide the order id!" });
+      const id = data;
+      if (!id) return NextResponse.json({ success: false, message: "Please provide the order id!" });
 
-      const saveData = await Order.findOneAndUpdate(id , { isDelivered : true }  , { new: true });
+      const saveData = await Order.findOneAndUpdate(id, { isDelivered: true }, { new: true });
 
       if (saveData) {
-
         return NextResponse.json({ success: true, message: "Order status updated successfully!" });
-
       } else {
-
         return NextResponse.json({ success: false, message: "Failed to update the Order status. Please try again!" });
-
       }
 
     } else {
-
       return NextResponse.json({ success: false, message: "You are not authorized." });
-
     }
 
   } catch (error) {
-
     console.log('Error in update order status:', error);
     return NextResponse.json({ success: false, message: 'Something went wrong. Please try again!' });
-
   }
 }
