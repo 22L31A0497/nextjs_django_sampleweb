72ef5d33718b97f616b57291dd74a3eea00c500a
Kuna Jaganmohan Rao <145915079+22L31A0497@users.noreply.github.com>
2025-08-31 21:14:53 +0530

    Update route.ts

--- src/app/api/Admin/order/update-order-status/route.ts
+++ src/app/api/Admin/order/update-order-status/route.ts
@@ -1,4 +1,4 @@
-import connectDB from "@/lib/connectDB";
+import connectDB from "../../../../../lib/connectDB";
 import AuthCheck from "../../../../../middleware/AuthCheck";
 import { NextResponse } from "next/server";
 import Order from "../../../../../model/Order";
